2f29adec8f4a1142de86367f4e787fd351ea10d9
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2022-08-20 13:18:10 -0300

    feat: add tailwindcss to the project

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,9 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" class="" src="./assets/logo.png">
+  <div class="flex justify-center text-center">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,14 +17,8 @@ import HelloWorld from './components/HelloWorld.vue';
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css">
+@import '@/assets/styles/main.scss';
 </style>
+
+
